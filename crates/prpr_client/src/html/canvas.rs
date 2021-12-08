d92d26d6a6002ac56f5e348bf76950b18c2c6914
murata <murata@kmc.gr.jp>
2021-12-09 03:51:01 +0900

    iikanji

--- crates/prpr_client/src/html/canvas.rs
+++ crates/prpr_client/src/html/canvas.rs
@@ -1,5 +1,6 @@
 pub trait Canvas {
   fn get_2d_context(&self) -> web_sys::CanvasRenderingContext2d;
+  fn get_webgl_context(&self) -> web_sys::WebGlRenderingContext;
 }
 impl Canvas for web_sys::HtmlCanvasElement {
   fn get_2d_context(&self) -> web_sys::CanvasRenderingContext2d {
@@ -9,5 +10,13 @@ impl Canvas for web_sys::HtmlCanvasElement {
       .expect("failed to get context 2d");
     wasm_bindgen::JsCast::dyn_into::<web_sys::CanvasRenderingContext2d>(context)
       .expect("failed to cast to CanvasRenderingContext2d")
+  }
+  fn get_webgl_context(&self) -> web_sys::WebGlRenderingContext {
+    let context = self
+      .get_context("webgl")
+      .expect("failed to get webgl context")
+      .expect("failed to get webgl context");
+    wasm_bindgen::JsCast::dyn_into::<web_sys::WebGlRenderingContext>(context)
+      .expect("failed to cast to WebGlRenderingContext")
   }
 }
