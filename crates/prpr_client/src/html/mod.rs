adb83f2f8ab80948851b384a4cc83db0512a271e
murata <murata@kmc.gr.jp>
2021-12-09 03:38:45 +0900

    crates

--- crates/prpr_client/src/html/mod.rs
+++ crates/prpr_client/src/html/mod.rs
@@ -1,32 +1,4 @@
 pub mod canvas;
 pub use canvas::*;
-
-pub fn window() -> web_sys::Window {
-  web_sys::window().expect("no global `window` exists")
-}
-pub fn document() -> web_sys::Document {
-  window()
-    .document()
-    .expect("should have a document on window")
-}
-pub fn body() -> web_sys::HtmlElement {
-  document().body().expect("document should have a body")
-}
-pub fn append_tag(parent: &web_sys::HtmlElement, tag: &str) -> web_sys::Node {
-  let created = document().create_element(tag).unwrap();
-  parent
-    .append_child(&created)
-    .expect(&format!("failed to append child ({})", tag))
-}
-pub fn append_div(parent: &web_sys::HtmlElement) -> web_sys::HtmlDivElement {
-  let div = append_tag(parent, "div");
-  wasm_bindgen::JsCast::dyn_into::<web_sys::HtmlDivElement>(div).expect("failed cast to div")
-}
-pub fn append_canvas(parent: &web_sys::HtmlElement) -> web_sys::HtmlCanvasElement {
-  let canvas = append_tag(parent, "canvas");
-  wasm_bindgen::JsCast::dyn_into::<web_sys::HtmlCanvasElement>(canvas)
-    .expect("failed cast to canvas")
-}
-pub fn create_root() -> web_sys::HtmlDivElement {
-  append_div(&body())
-}
+pub mod element;
+pub use element::*;
