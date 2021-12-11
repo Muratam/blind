f9c7d4ceaf17399b78fe645d5c1dc25eabc61da7
murata <murata@kmc.gr.jp>
2021-12-11 21:28:56 +0900

    renderpass

--- crates/prpr_client/src/lib.rs
+++ crates/prpr_client/src/lib.rs
@@ -24,7 +24,6 @@ impl System for SampleSystem {
     let surface = prgl.new_surface();
     let mut renderpass = prgl.new_renderpass();
     renderpass.set_color_target(&surface);
-    renderpass.set_color_target(&surface);
     let pipeline = prgl.new_pipeline();
     Self {
       surface,
@@ -37,7 +36,9 @@ impl System for SampleSystem {
     {
       // TODO: GLの update までの流れは別途モジュール化する
       let v = ((frame as f32) / 100.0).sin() * 0.25 + 0.75;
-      self.renderpass.set_clear_color(Vec4::new(v, v, v, 1.0));
+      self
+        .renderpass
+        .set_clear_color(Some(Vec4::new(v, v, v, 1.0)));
       self.renderpass.bind();
       self.pipeline.draw();
       core.get_main_prgl().swap_surface(&self.surface);
--- crates/prpr_client/src/prgl/renderpass.rs
+++ crates/prpr_client/src/prgl/renderpass.rs
@@ -1,65 +1,65 @@
 use super::*;
-// for renderpass types
-// struct RawFrameBuffer {}
-// struct RawRenderBuffer {}
-// struct RawColorRenderTarget {
-//   texture: Option<RawTexture>,
-//   frame_buffer: RawFrameBuffer,
-//   clear_value: Vec4,
-//   is_clear: bool,
-// }
-// struct RawDepthRenderTarget {
-//   texture: Option<RawTexture>,
-//   clear_value: f32,
-//   is_clear: bool,
-// }
-// struct RawStencilRenderTarget {
-//   texture: Option<RawTexture>,
-//   clear_value: i64,
-//   is_clear: bool,
-// }
-// struct RawRenderPass {
-//   color_attachments: Vec<RawColorRenderTarget>,
-//   depth_attachment: RawDepthRenderTarget,
-//   stencil_attachment: RawStencilRenderTarget,
-//   frame_buffer: RawFrameBuffer,
-//   render_buffer: RawRenderBuffer,
-//   viewport: Option<Rect<f32>>,
-//   scissor: Option<Rect<i32>>,
-// }
 
 pub struct RenderPass {
   gl: Rc<WebGlContext>,
-  clear_colors: [Vec4; MAX_OUTPUT_SLOT],
+  clear_colors: [Option<Vec4>; MAX_OUTPUT_SLOT],
+  clear_depth: Option<f32>,
+  clear_stencil: Option<i32>,
+  // frame_buffer: RawFrameBuffer,
+  // render_buffer: RawRenderBuffer,
+  // viewport: Option<Rect<f32>>,
+  // scissor: Option<Rect<i32>>,
+  // color_targets: Vec<RawTexture>,
+  // depth_target: RawTexture,
+  // stencil_target: RawStencRawTextureilRenderTarget,
 }
 impl RenderPass {
   pub fn new(gl: Rc<WebGlContext>) -> RenderPass {
     RenderPass {
       gl: Rc::clone(&gl),
-      clear_colors: [Vec4::ZERO; MAX_OUTPUT_SLOT],
+      clear_colors: [None; MAX_OUTPUT_SLOT],
+      clear_depth: None,
+      clear_stencil: None,
     }
   }
   pub fn bind(&self) {
     let gl = &self.gl;
-    // TODO: 今はゼロスロット目のみなのをなおす
-    let color = self.clear_colors[0];
-    gl.clear_color(color.x, color.y, color.z, color.w);
-    gl.clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+    // TODO: 今はゼロスロット目のみ
+    let mut flag = 0;
+    if let Some(color) = self.clear_colors[0] {
+      gl.clear_color(color.x, color.y, color.z, color.w);
+      flag |= gl::COLOR_BUFFER_BIT;
+    }
+    if let Some(depth) = self.clear_depth {
+      gl.clear_depth(depth);
+      flag |= gl::DEPTH_BUFFER_BIT;
+    }
+    if let Some(stencil) = self.clear_stencil {
+      gl.clear_stencil(stencil);
+      flag |= gl::STENCIL_BUFFER_BIT;
+    }
+    gl.clear(flag);
   }
   pub fn set_color_target(&mut self, target: &Texture) {
-    log::error("set_color_target: not implemented");
+    self.set_color_target_by_slot(target, 0);
   }
   pub fn set_color_target_by_slot(&mut self, target: &Texture, slot: i32) {
     log::error("set_color_target_by_slot: not implemented");
   }
-  pub fn set_clear_color(&mut self, clear_color: Vec4) {
-    self.set_clear_color_by_slot(clear_color, 0);
+  pub fn set_clear_color(&mut self, value: Option<Vec4>) {
+    self.set_clear_color_by_slot(value, 0);
   }
-  pub fn set_clear_color_by_slot(&mut self, clear_color: Vec4, slot: i32) {
+  pub fn set_clear_depth(&mut self, value: Option<f32>) {
+    self.clear_depth = value;
+  }
+  pub fn set_clear_stencil(&mut self, value: Option<i32>) {
+    self.clear_stencil = value;
+  }
+  pub fn set_clear_color_by_slot(&mut self, value: Option<Vec4>, slot: i32) {
     if slot < 0 || slot >= MAX_OUTPUT_SLOT as i32 {
       log::error(format!("Invalid SetClearColor Slot {}", slot));
       return;
     }
-    self.clear_colors[slot as usize] = clear_color;
+    self.clear_colors[slot as usize] = value;
   }
 }
